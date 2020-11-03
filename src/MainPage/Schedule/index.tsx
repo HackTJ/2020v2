4d3dfcfae3c9831d962d8141c1eba5142e23cb0a
Sumanth Ratna <sumanthratna@gmail.com>
2020-11-03 09:11:33 -0500

    Update schedule

--- src/MainPage/Schedule/index.tsx
+++ src/MainPage/Schedule/index.tsx
@@ -17,81 +17,55 @@ const Schedule = (): JSX.Element => (
          className="vertical-segment green"
          style={{ left: "-5.9%", top: "-3.4%" }}
         ></div> */}
-        <h1 className="section-title">Last Year&#39;s Schedule</h1>
+        <h1 className="section-title">Schedule</h1>
+        <h2 className="day-title">Saturday, December 12</h2>
         <ul className="day">
-          <div className="day-title">
-            <h3>Saturday, March 21</h3>
-          </div>
           <li className="item">
             <h6 className="time">10:00am</h6>
-            <div className="description">Student Check-In & Sponsor Fair</div>
-          </li>
-          <li className="item">
-            <h6 className="time">11:00am</h6>
             <div className="description">Opening Ceremony</div>
           </li>
           <li className="item">
-            <h6 className="time">11:30am</h6>
+            <h6 className="time">11:00am</h6>
             <div className="description">Hacking Begins</div>
-          </li>
-          <li className="item">
-            <h6 className="time">1:00pm</h6>
-            <div className="description">
-              Lunch, Registration Closes (no entry after this time)
-            </div>
           </li>
           <li className="item">
             <h6 className="time">2:00pm</h6>
             <div className="description">Workshops Begin</div>
           </li>
           <li className="item">
-            <h6 className="time">4:00pm</h6>
-            <div className="description">Afternoon Snack</div>
+            <h6 className="time">2:30pm</h6>
+            <div className="description">TypeRacer Tournament</div>
+          </li>
+          <li className="item">
+            <h6 className="time">4:30pm</h6>
+            <div className="description">Chess Blitz</div>
           </li>
           <li className="item">
             <h6 className="time">7:00pm</h6>
-            <div className="description">Dinner</div>
+            <div className="description">Board Game Emulator</div>
           </li>
           <li className="item">
-            <h6 className="time">9:00pm</h6>
-            <div className="description">Women in Tech Panel</div>
+            <h6 className="time">8:00pm</h6>
+            <div className="description">MLH Mini-Event</div>
           </li>
         </ul>
+        <h2 className="day-title">Sunday, December 13</h2>
         <ul className="day">
-          <div className="day-title">
-            <h3>Sunday, March 22</h3>
-          </div>
           <li className="item">
-            <h6 className="time">12:00am</h6>
-            <div className="description">Midnight Snack</div>
+            <h6 className="time">2:00pm</h6>
+            <div className="description">Pictionary</div>
           </li>
           <li className="item">
-            <h6 className="time">4:00am</h6>
-            <div className="description">Late Night Snack</div>
+            <h6 className="time">5:00pm</h6>
+            <div className="description">Hacking Ends</div>
           </li>
           <li className="item">
-            <h6 className="time">8:00am</h6>
-            <div className="description">Hacking Ends & Breakfast Begins</div>
-          </li>
-          <li className="item">
-            <h6 className="time">8:45am</h6>
-            <div className="description">Clean-Up</div>
-          </li>
-          <li className="item">
-            <h6 className="time">9:30am</h6>
+            <h6 className="time">5:30pm</h6>
             <div className="description">Judging Starts</div>
           </li>
           <li className="item">
-            <h6 className="time">11:00am</h6>
-            <div className="description">People&apos;s Choice Voting</div>
-          </li>
-          <li className="item">
-            <h6 className="time">11:30am</h6>
+            <h6 className="time">8:30pm</h6>
             <div className="description">Closing Ceremony</div>
-          </li>
-          <li className="item">
-            <h6 className="time">12:00pm</h6>
-            <div className="description">Hackathon Ends</div>
           </li>
         </ul>
       </div>
