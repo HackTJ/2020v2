9e6fba05ec0ac06c1f2eb5174e7c6705d139be0b
Sumanth Ratna <sumanthratna@gmail.com>
2020-11-03 12:44:02 -0500

    Revert "Add jumbotron banner for registration"
    This reverts commit c43011e7b1b10caa21488af50d8cb1685ae2a932.

--- src/MainPage/Landing/index.tsx
+++ src/MainPage/Landing/index.tsx
@@ -1,6 +1,5 @@
 import * as React from "react";
 
-import { Jumbotron, Container } from "reactstrap";
 import Tilt from "react-parallax-tilt";
 import { Link } from "react-router-dom";
 
@@ -10,52 +9,43 @@ import "./index.css";
 const logo = require("../../images/bigweblogo.png");
 
 const Landing = (): JSX.Element => (
-  <>
-    <Jumbotron fluid>
-      <Container fluid style={{ paddingTop: "10px", paddingLeft: "10px" }}>
-        <h1>Registration is open!</h1>
-        Registration opened on November 2nd, 2020 at 8 p.m., and will remain
-        open until November 13, 2020 at 8 p.m.
-      </Container>
-    </Jumbotron>
-    <section className="hero">
-      <div className="container">
-        <Tilt
-          className="event-container"
-          trackOnWindow
-          gyroscope
-          tiltMaxAngleX={5}
-          tiltMaxAngleY={5}
-        >
-          {/* <Logo className="logo" /> */}
-          <img className="logo" src={String(logo)} alt="HackTJ" />
-          <div className="event-date landing">December 12th-13th, 2020</div>
-          <div className="event-location landing">Online</div>
-          <div className="register-button-block-old top">
-            <Link
-              className="button register-button mentor blackbutton"
-              to="registration"
-            >
-              Register for HackTJ 7.5
-            </Link>
-            <a
-              className="button register-button mentor blackbutton"
-              href="https://www.facebook.com/pg/HackTJOfficial/photos/?tab=album&album_id=2168769876535523"
-            >
-              2019 Pictures
-            </a>
-            <Link
-              className="button register-button mentor blackbutton"
-              to="history"
-            >
-              Past Projects
-            </Link>
-            {/* <a className="button register-button mentor" href="https://hacktj2019.devpost.com/">Devpost</a> */}
-          </div>
-        </Tilt>
-      </div>
-    </section>
-  </>
+  <section className="hero">
+    <div className="container">
+      <Tilt
+        className="event-container"
+        trackOnWindow
+        gyroscope
+        tiltMaxAngleX={5}
+        tiltMaxAngleY={5}
+      >
+        {/* <Logo className="logo" /> */}
+        <img className="logo" src={String(logo)} alt="HackTJ" />
+        <div className="event-date landing">December 12th-13th, 2020</div>
+        <div className="event-location landing">Online</div>
+        <div className="register-button-block-old top">
+          <Link
+            className="button register-button mentor blackbutton"
+            to="registration"
+          >
+            Register for HackTJ 7.5
+          </Link>
+          <a
+            className="button register-button mentor blackbutton"
+            href="https://www.facebook.com/pg/HackTJOfficial/photos/?tab=album&album_id=2168769876535523"
+          >
+            2019 Pictures
+          </a>
+          <Link
+            className="button register-button mentor blackbutton"
+            to="history"
+          >
+            Past Projects
+          </Link>
+          {/* <a className="button register-button mentor" href="https://hacktj2019.devpost.com/">Devpost</a> */}
+        </div>
+      </Tilt>
+    </div>
+  </section>
 );
 
 export default Landing;
